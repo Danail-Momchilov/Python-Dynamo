b941138606c7888ad1e8bd61be4df5cd70db87eb
Danail Momchilov <46314846+Danail-Momchilov@users.noreply.github.com>
2023-02-09 17:40:13 +0200

    Update IntersectCurves.py

--- Structural Openings/IntersectCurves.py
+++ Structural Openings/IntersectCurves.py
@@ -21,14 +21,6 @@ clr.AddReference("RevitAPI")
 import Autodesk
 from Autodesk.Revit.DB import Curve
 
-def compareListsSorted(list0, list1):
-	doesContain = True
-	sortLst = sorted(list1)
-	for list in list0:
-		if sorted(list) == sorted(sortLst):
-			doesContain = False
-	return doesContain
-
 curves = UnwrapElement(IN[0])
 outlist = []
 
@@ -37,14 +29,19 @@ for list in curves:
 	for i, curve0 in enumerate(list):
 		intersectsublist = []
 		for j, curve1 in enumerate(list):
-			if i != j:
-				if curve0.Intersect(curve1):
-					if i not in intersectsublist:
-						intersectsublist.append(i)
-					intersectsublist.append(j)
+			if curve0.Intersect(curve1):
+				if i not in intersectsublist:
+					intersectsublist.append(i)
+				intersectsublist.append(j)
 		if intersectsublist:
-			if compareListsSorted(intersectlist, intersectsublist):
-				intersectlist.append(intersectsublist)
+			test = True
+			for l in intersectlist:
+				if any(elem in l for elem in intersectsublist):
+					l += intersectsublist
+					test = False
+					break
+			if test:
+				intersectlist.append(intersectsublist)			
 	outlist.append(intersectlist)
 				
 OUT = outlist
